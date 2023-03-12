d54d65806de5f840eb2e24a0b58b5f8e26985ad0
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-03-12 23:54:07 +0300

    Add files via upload

---
+++ fibonacci_function/src/fibonacci_function/fibonacci_function.java
@@ -0,0 +1,22 @@
+package fibonacci_function;
+
+public class fibonacci_function {
+	public static void main(String args[]) {
+		int sayi = 11;
+
+		FibHesapla(sayi);
+	}
+
+	static void FibHesapla(int sayi) {
+		int n1 = 1, n2 = 1, n3;
+		System.out.print(n1 + " " + n2);
+
+		for (int i = 2; i < sayi; ++i) {
+			n3 = n1 + n2;
+			System.out.print(" " + n3);
+			n1 = n2;
+			n2 = n3;
+		}
+
+	}
+}
