a547afc1f3b1d5c4d0dcf174fc0e4db317f78af3
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-03-15 23:56:29 +0300

    Add files via upload

---
+++ fermat_sayi/src/fermat_sayi.java
@@ -0,0 +1,11 @@
+
+public class fermat_sayi {
+	public static void main(String[] args) {
+		int fermat;
+		for (int i = 0; i < 6; i++) {
+			fermat = (int) (Math.pow(2, Math.pow(2, i)) + 1);
+			System.out.println(fermat);
+		}
+	}
+
+}
